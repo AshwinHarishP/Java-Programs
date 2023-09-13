f19b9e0513088e4274d3705c5fc34d4e6e8ef224
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-13 23:19:15 +0530

    Add files via upload

---
+++ Ms_ford.java
@@ -0,0 +1,8 @@
+public class Ms_ford {
+    public static void main(String[] args) {
+        int [] arr = {60,30,40,60,10,20};
+        for (int i = arr.length -3; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
