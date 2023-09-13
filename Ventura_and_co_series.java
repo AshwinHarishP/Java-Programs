244495b54346064079993a3e3cd3b6dae065ffb6
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-13 23:18:47 +0530

    Add files via upload

---
+++ Ventura_and_co_series.java
@@ -0,0 +1,7 @@
+// Ventura and co has series of book publication such as shreys, suhas, vikas, playas
+public class Ventura_and_co_series {
+    public static void main(String[] args) {
+        String [] publications = {"shreys", "suhas", "vikas", "playas"};
+        System.out.println(publications.length);
+    }
+}
