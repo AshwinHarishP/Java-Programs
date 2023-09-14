b5995c7c7b643f167d26dff25f7130cc3e3305d8
Ashwin Harish.P <99186533+AshwinHarishP@users.noreply.github.com>
2023-09-14 23:32:40 +0530

    Add files via upload

---
+++ Simple_constructor.java
@@ -0,0 +1,10 @@
+class Bike1{
+    Bike1(){
+        System.out.println("This is Bike");
+    }
+}
+public class Simple_constructor {
+    public static void main(String[] args) {
+        Bike1 obj = new Bike1();
+    }    
+}
